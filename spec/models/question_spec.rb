6ef5d156a0f325595d5b82053112da7ba025fca9
Danya D. Smith <danyadsmith@gmail.com>
2015-09-25 09:51:43 -0400

    Tests models for Questions and Answers and updates associations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: false)}
+
+  describe "attributes" do
+
+    it "should respond to title" do
+      expect(post).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
+
+    it "should respond to resolved" do
+      expect(post).to respond_to(:resolved)
+    end
+  
+  end
 end
