074d5194bcc96d9f39cbfc60f807a342f0c753ce
Danya D. Smith <danyadsmith@gmail.com>
2015-09-25 21:16:18 -0400

    Seeds the database with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include RandomData
+
+50.times do
+  
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+post = Post.find_or_create_by(title: "Bloc", body: "The Bloc program is intense!")
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+comment = Comment.find_or_create_by(post: post, body: "This is my assignment comment.")
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
