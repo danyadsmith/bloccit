d0a3596b16f710b2464e26e49f3a1bdc71e4fc7e
Danya D. Smith <danyadsmith@gmail.com>
2015-10-04 11:57:42 -0400

    Adds form validation to topics, posts and sponsored posts
    https://www.bloc.io/users/danya-smith/checkpoints/1647?roadmap_section=120
    
    To prevent the database from being cluttered with invalid data, we add validation to the topics, posts and sponsored posts forms. To refactor the code and make it more DRY, we move each form definition to a partial and include the simple_form gem.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :sponsored_posts, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, length: {minimum: 5}, presence: true
+  validates :description, length: {minimum: 15}, presence: true  
 end
