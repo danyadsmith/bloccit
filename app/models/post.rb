9e7fd08726092554e5e9a3025d907312f75079de
Danya D. Smith <danyadsmith@gmail.com>
2015-10-07 22:47:20 -0400

    Adds Scopes to the Posts Model
    https://www.bloc.io/users/danya-smith/checkpoints/1643?roadmap_section=120
    
    Allows posts to be returned sorted in ascending order by title.
    Allows posts to be returned sorted in ascending order by created date.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-
+  
+  scope :ordered_by_title, -> { Post.unscoped.order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { Post.unscoped.order('created_at ASC') }
   default_scope { order('created_at DESC') }
   
   validates :title, length: {minimum: 5}, presence: true
