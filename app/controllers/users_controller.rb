c645868fde7437d28510d54d62ad3b11630498f0
Danya D. Smith <danyadsmith@gmail.com>
2015-10-31 16:59:39 -0400

    Adds gravatar images to Post listings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.visible_to(current_user)
+    favorites = []
+    @user.favorites.each do |favorite|
+      favorites << favorite.post_id
+    end
+    @favorite_posts = Post.find(favorites)
   end
 
   def create
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def has_authored_posts?(user)
+    user.posts.count > 0 ? true : false
+  end
+
+  def has_authored_comments?(user)
+    user.comments.count > 0 ? true : false
+  end
+
+  def has_favorites?(user)
+    user.favorites.count > 0 ? true : false
+  end
 end
